280864b0b5317a416421af45aeeaaa8c9896ab86
nikhil3542 <78140422+Nikhil5598@users.noreply.github.com>
2024-09-30 18:18:40 +0530

    Implemented Interfaces

--- Basic.java
+++ Basic.java
@@ -1,6 +1,16 @@
-class Basic{
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+interface Nikhil{
+    void show();
+}
+class Basic implements Nikhil{
+    
+       
+        public void show(){
+            System.out.println("Hello");
+        }
+        public static void main(String[] args) {
+        Basic sc=new Basic();
+        sc.show();
+       
         // java branch
     }
 }
